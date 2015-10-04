e4c4fc79765cd82b76050f5f663774e907e040db
Masakaze <redkingslime@gmail.com>
2015-10-04 21:59:27 +0900

    修正

--- app/models/game_info.rb
+++ app/models/game_info.rb
@@ -2,5 +2,5 @@ class GameInfo < ActiveRecord::Base
 #  attr_accessible :name_jp, :name_en
 #  attr_accessible :game_genre_id
 
-  has_many_and_belongs_to :game_genres
+  has_and_belongs_to_many :game_genres
 end
