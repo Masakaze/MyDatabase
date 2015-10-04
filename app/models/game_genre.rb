6ad6f6bfdcf8f4a3a956b02b8e3a0c7e84cf46f9
Masakaze <redkingslime@gmail.com>
2015-10-04 21:39:50 +0900

    attr_accessorとattr_accesibleをごっちゃにしてた+Rail4.0でattr_accessibleが使えなくなっていたので修正

--- app/models/game_genre.rb
+++ app/models/game_genre.rb
@@ -1,5 +1,6 @@
 class GameGenre < ActiveRecord::Base
-  attr_accessor :name_jp, :name_en
+#  attr_accessible :name_jp, :name_en
 
   has_many :game_infos
+
 end
--- app/models/game_info.rb
+++ app/models/game_info.rb
@@ -1,6 +1,6 @@
 class GameInfo < ActiveRecord::Base
-  attr_accessor :name_jp, :name_en
-  attr_accessor :game_genre_id
+#  attr_accessible :name_jp, :name_en
+#  attr_accessible :game_genre_id
 
   belongs_to :game_genre
 end
