aaac575b90a6b8a8e4767af14a119c7282175da7
Masakaze <redkingslime@gmail.com>
2015-10-04 21:30:56 +0900

    GameGenreのseedを書いたけど更新がうまくいかない。。。

--- app/models/game_genre.rb
+++ app/models/game_genre.rb
@@ -1,2 +1,5 @@
 class GameGenre < ActiveRecord::Base
+  attr_accessor :name_jp, :name_en
+
+  has_many :game_infos
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# GameGenreの初期化
+init_game_genres = [
+                   {:name_jp => "アクション", :name_en => "Action"},
+                   {:name_jp => "RPG", :name_en => "RPG"},
+                  ]
+init_game_genres.each { |info|
+  genre = GameGenre.find_by(name_en: info[:name_en])
+  next if genre != nil
+
+  genre = GameGenre.new(name_en: info[:name_en], name_jp: info[:name_jp])
+  genre.name_jp = info[:name_jp]
+
+  if genre.save == false
+    abort("GameGenre:#{genre.name_en}の初期化に失敗")
+  else
+    puts "Add Genre"
+    p genre
+  end
+}
