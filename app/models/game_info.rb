19b7b58cfcfcfc3c49af3601cb897a395eaa9c6f
Masakaze <redkingslime@gmail.com>
2015-10-12 15:06:09 +0900

    GameInfoにname_jpのpresence検証追加。

--- app/models/game_info.rb
+++ app/models/game_info.rb
@@ -2,6 +2,8 @@ class GameInfo < ActiveRecord::Base
 #  attr_accessible :name_jp, :name_en
 #  attr_accessible :game_genre_id
 
+  validates :name_jp, presence: true
+
   has_and_belongs_to_many :game_genres
   has_and_belongs_to_many :game_platforms
 end
--- spec/requests/game_infos_spec.rb
+++ spec/requests/game_infos_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe "GameInfos" do
-=begin
-  describe "GET /game_infos" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get game_infos_index_path
-      response.status.should be(200)
-    end
-  end
-=end
-
   describe "TopPage" do
     it "TopPage's content should be" do
       visit '/game_infos'
       expect(page).to have_content("ゲームデータベース")
       expect(page).to have_title("GameDataBase")
+    end
+  end
+
+  describe "GameInfoRegister" do
+    before do
+      @game_info = GameInfo.new(name_jp: "テストゲーム")
+    end
+
+    subject { @game_info}
+    it { should respond_to(:name_jp) }
+
+    it { should be_valid }
+
+    describe "when name_jp is not presence" do
+      before { @game_info.name_jp = "" }
+      it { should_not be_valid }
     end
   end
 end
