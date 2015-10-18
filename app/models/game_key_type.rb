268b87e6bf07b5bfd7f4cb530de68d81619a014a
Masakaze <redkingslime@gmail.com>
2015-10-18 20:28:00 +0900

    GameKeyTypeでgame_platform_idとname_enの組み合わせで重複チェックするように

--- app/models/game_key_type.rb
+++ app/models/game_key_type.rb
@@ -4,4 +4,7 @@ class GameKeyType < ActiveRecord::Base
   has_many :game_keys
 
   validates :game_platform_id, presence: true
+  # name_enとgame_platform_idの組み合わせでuniqueチェック 必要があったらマイグレーションに下記追加
+  # add_index :game_key_types, [:name_en, :game_platform_id], :unique => true
+  validates :name_en, :uniqueness => {:scope => [:game_platform_id]}
 end
--- spec/models/game_infos_spec.rb
+++ spec/models/game_infos_spec.rb
@@ -13,6 +13,16 @@ describe "Game Key Type" do
     before { @game_key_type.game_platform_id = nil }
     it { should_not be_valid }
   end
+
+  describe "when game_platform_id and name_en is duplicate" do
+    before do
+      info = {:name_en => "FirstData", :game_platform_id => 1}
+      first_save_data = GameKeyType.find_or_create_by(:name_en => "FirstData", :game_platform_id => 1)
+      @game_key_type = GameKeyType.new(info)
+    end
+
+    it { should_not be_valid }
+  end
 end
 
 # GameKey
