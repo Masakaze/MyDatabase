afc3ec4f4813706fbf3dbe34a99bdc1941483fa6
Masakaze <redkingslime@gmail.com>
2015-11-04 23:49:50 +0900

    newページに見積もり時間のselectタグを追加

--- spec/requests/task_infos_spec.rb
+++ spec/requests/task_infos_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe "NewPage" do
+  it "NewPage have" do
+    visit '/task_infos/new'
 
+    # 見積もり時間のselectタグが存在
+    expect(find("select#estimate_time_select")).not_to eq nil
+  end
 end
 
 describe "ShowPage" do
