e140cbb4c6420b01e91bc7923ad1c62cc9fab8ed
Masakaze <redkingslime@gmail.com>
2015-11-05 00:28:42 +0900

    新規作成ページで正しい情報を入力してTaskInfoが登録されるテスト追加

--- spec/requests/task_infos_spec.rb
+++ spec/requests/task_infos_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe "NewPage" do
+
+  estimate_time_select_id = "estimate_time_select"
+  create_button = "Create Task info"
+
   it "NewPage have" do
-    visit '/task_infos/new'
+    visit new_task_info_path
 
     # 見積もり時間のselectタグが存在
-    expect(find("select#estimate_time_select")).not_to eq nil
+    expect(find("select##{estimate_time_select_id}")).not_to eq nil
+  end
+
+  it "Correct Info" do
+    visit new_task_info_path
+
+    fill_in "タイトル", with: "テスト"
+    fill_in "詳細", with: "テストの詳細"
+    select TaskTimeType.first.name_jp, from: estimate_time_select_id
+
+    expect { click_button create_button }.to change(TaskInfo, :count).by(1)
   end
 end
 
