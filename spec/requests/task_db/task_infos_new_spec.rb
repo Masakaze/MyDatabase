b5f254859cd778c79471c74f9c9a39b192c3c5d8
Masakaze <redkingslime@gmail.com>
2015-11-11 23:41:54 +0900

    TaskDBのnewページのテストをファイル分け

--- spec/requests/task_db/task_infos_new_spec.rb
+++ spec/requests/task_db/task_infos_new_spec.rb
@@ -1,15 +1,4 @@
-#
-# @brief	TaskDB全体のテスト
-# @note		task_dbフォルダに細かくファイル分けしてます
-# @note		「-e キーワード」オプションでitを部分実行できるみたいだけど、早くなるかはその時によりそう
-#
-
 require 'spec_helper'
-
-$:.unshift(File.join( File.dirname(__FILE__), "task_db") )
-require "task_infos_show_spec"
-require "task_infos_index_spec"
-require "task_infos_new_spec"
 
 describe "NewPage" do
 
@@ -39,5 +28,3 @@ describe "NewPage" do
 
   end
 end
-
-
