df13fc8d5bda4c033cd3898b1a2a261428ed9a40
Masakaze <redkingslime@gmail.com>
2015-11-18 01:38:02 +0900

    コメント追加

--- app/models/actor_base_info.rb
+++ app/models/actor_base_info.rb
@@ -1,3 +1,11 @@
+# Rails4での保存方法
+=begin
+params = ActionController::Parameters.new
+params[:actor_base_info] = {:relation_actor_base_id => actor_base.id, :relation_info_id => info.id}
+relation = ActorBaseInfo.new(params.require(:actor_base_info).permit(:relation_actor_base_id, :relation_info_id))
+=end
+
+
 class ActorBaseInfo < ActiveRecord::Base
 
   belongs_to :relation_actor_base, :class_name => "ActorBase", :foreign_key => "relation_actor_base_id"
--- app/models/info.rb
+++ app/models/info.rb
@@ -1,4 +1,4 @@
 class Info < ActiveRecord::Base
-  has_many :actor_base_info_relations, :class_name => "ActorBaseInfo", :foreign_key => "relation_info_id"
+  has_many :actor_base_info_relations, :class_name => "ActorBaseInfo", :foreign_key => "relation_info_id"#, :autosave => true
   has_many :relation_actor_bases, :class_name => "ActorBase", :through => :actor_base_info_relations, :source => "relation_actor_base"
 end
