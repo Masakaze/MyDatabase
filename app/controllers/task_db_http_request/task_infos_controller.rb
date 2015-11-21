1b8e5aff59cee0052ed58fe911934b05cca68384
Masakaze <redkingslime@gmail.com>
2015-11-21 10:12:42 +0900

    productionでpostをとりあえず実装

--- app/controllers/task_db_http_request/task_infos_controller.rb
+++ app/controllers/task_db_http_request/task_infos_controller.rb
@@ -7,14 +7,25 @@ class TaskDbHttpRequest::TaskInfosController < ApplicationController
     task_info.detail = params[:detail] if params[:detail]!= nil
     task_info.save
 
-    redirect_to task_info_path(task_info)
+    require 'yaml'
+    respond_to do |format|
+      format.html { render :text => params.to_yaml }
+    end
   end
 
   def create
-    task_info = TaskInfo.find_by(:task_category_id => TaskCategory.task_category_test.id)
-    task_info.detail = "スクリプトからのPostテスト"
+    task_info = TaskInfo.find(params[:id])
+    abort if task_info == nil
+    task_info_log = nil
+    if params[:task_info_logs] != nil && params[:task_info_logs][:content] != nil
+      task_info_log = TaskInfoLog.create(:content => params[:task_info_logs][:content], :task_info_id => params[:id])
+    end
+    abort if task_info_log == nil
+
+    task_info.task_info_logs << task_info_log
     task_info.save
 
     head :ok
   end
+
 end
