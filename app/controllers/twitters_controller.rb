488268383606e21da15cd1a258c4270a9cc0833d
Masakaze <redkingslime@gmail.com>
2016-01-03 23:19:31 +0900

    ホームのタイムライン取得も実装

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -6,8 +6,27 @@ class TwittersController < ApplicationController
 
   include Twitter::TwitterUtil
 
+  before_action :login_twitter, :only => ['get_home_timeline', 'post_tweet_by_get']
+
   def index
     render
+  end
+
+  # GET twitters/get_home_timeline.xml
+  def get_home_timeline
+    home_timelines = get_home_timelines(@accessor)
+    result = []
+    home_timelines.each { |home_timeline|
+      result << {
+        :id => home_timeline.id,
+        :name => home_timeline.name,
+        :text => home_timeline.text,
+      }
+    }
+
+    respond_to do |format|
+      format.xml { render :xml => result }
+    end
   end
 
   # GET
@@ -15,14 +34,6 @@ class TwittersController < ApplicationController
     # params[:tweet_text] = "Rails(Get)から投稿するテスト"
     # params[:reply_id] = "683645191547310080"
     tweet_text = params[:tweet_text]
-
-    accessor = TwitterAccessor.new()
-    accessor.login(
-                   "dFvUnUTuJFI4f8qsXoNW64ZBb",
-                   "dA0kcMZO4Tx5TFVFa7E9wXbSgsI2K0FnwYc9l4DUsNvJGBi88W",
-                   "4667929753-7MpXsp7HjgxjQeHcW5H3l69sX3dSTtw3PSNEOMy",
-                   "SUZXkAHGtqsR2othTAIbVM48MmzKyPsAovUk0Zc2srVTz"
-                   )
 
     post_options = {}
     if params.key?(:reply_id)
@@ -32,9 +43,20 @@ class TwittersController < ApplicationController
       post_options[:in_reply_to_status_id] = params[:reply_id]
     end
 
-    Twitter::TwitterUtil::post(accessor, tweet_text, post_options)
+    Twitter::TwitterUtil::post(@accessor, tweet_text, post_options)
 
     render :text => "success"
+  end
+
+  private
+  def login_twitter
+    @accessor = TwitterAccessor.new()
+    @accessor.login(
+                   "dFvUnUTuJFI4f8qsXoNW64ZBb",
+                   "dA0kcMZO4Tx5TFVFa7E9wXbSgsI2K0FnwYc9l4DUsNvJGBi88W",
+                   "4667929753-7MpXsp7HjgxjQeHcW5H3l69sX3dSTtw3PSNEOMy",
+                   "SUZXkAHGtqsR2othTAIbVM48MmzKyPsAovUk0Zc2srVTz"
+                   )
   end
 
 end
