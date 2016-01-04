82067c853feb00fd49fece8fd2faa358bbdb10d4
Masakaze <redkingslime@gmail.com>
2016-01-04 14:42:00 +0900

    pythonスクリプトでファイル入出力できるか確認

--- lib/python/sample.py
+++ lib/python/sample.py
@@ -5,5 +5,19 @@
 # @brief pythonのサンプルスクリプト
 #
 
+import os
+import datetime
+
 if __name__ == '__main__':
-    print("python script output")
+    tmp_file = os.path.join(os.path.dirname(__file__), "sample.txt")
+    print("file out start")
+    with open(tmp_file, "w") as fout:
+        fout.write(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    print("file out end")
+
+    print("file read start")
+    with open(tmp_file, "r") as fin:
+        print(fin.read())
+    print("file read end")
+
+    
